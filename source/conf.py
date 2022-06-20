15186f4356ed73818b8c88b02bf25d6d939311ba
likefallwind <likefallwind@163.com>
2022-06-20 19:24:40 +0800

    change conf.py

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,11 @@ author = 'Yibo Wang'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+        'myst_parser',
+        'deepmodeling_sphinx',
+]
+myst_enable_extensions = [
+    'dollarmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +49,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js'
